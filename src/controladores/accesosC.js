7a3f3c4d51c9bb6dd8b1c69356db295d2f79a817
KSoriano2 <sorianog500@gmail.com>
2025-06-10 00:20:57 -0500

    Update accesosC.js

--- src/controladores/accesosC.js
+++ src/controladores/accesosC.js
@@ -1,38 +1,4 @@
-export const recuperarMenu = async (req, res) => {
-  const { usr_usuario } = req.body;
-
-  if (!usr_usuario) {
-    return res.status(400).json({ error: "Usuario requerido" });
-  }
-
-  try {
-    
-    const [userRows] = await conmysql.query(
-      "SELECT ID_PERFIL FROM USUARIOS WHERE USR_USUARIO = ?",
-      [usr_usuario]
-    );
-
-    if (userRows.length === 0) {
-      return res.status(404).json({ error: "Usuario no encontrado" });
-    }
-
-    const per_id = userRows[0].PER_ID;
-
-    // 2. Obtener accesos según PER_ID
-    const [menuRows] = await conmysql.query(
-      "SELECT ACCESO_NOMBRE, ACCESO_PAGINA FROM ACCESOS WHERE PERFIL_ID = ? AND ACCESO_ESTADO = 'A'",
-      [per_id]
-    );
-
-    res.json({ cant: menuRows.length, data: menuRows })
-  } catch (error) {
-    console.error("Error al recuperar menú:", error);
-    return res.status(500).json({ error: "Error al recuperar el menú" });
-  }
-};
-
-
-//Métodos para Accesos
+import {conmysql} from "../bd.js";
 
 export const obtenerAccesos = (req, resp)=>{
     resp.send('Lista de Instituciones Especialidad')
