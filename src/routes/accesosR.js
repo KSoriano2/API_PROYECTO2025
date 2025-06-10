91d6ba906d550f35db7778ff9061f8b899f493fa
KSoriano2 <sorianog500@gmail.com>
2025-06-10 00:23:27 -0500

    Update accesosR.js

--- src/routes/accesosR.js
+++ src/routes/accesosR.js
@@ -1,10 +1,9 @@
 import { Router } from "express";
-import { recuperarMenu, getAccesos, getAccesoxid, postAcceso, putAcceso, patchAcceso, deleteAccesoxid } from "../controladores/accesosC.js";
+import { getAccesos, getAccesoxid, postAcceso, putAcceso, patchAcceso, deleteAccesoxid } from "../controladores/accesosC.js";
 
 
 const router = Router();
 
-router.post('/menu', recuperarMenu);
 router.get('/accesos', getAccesos);
 router.get('/accesos/:id', getAccesoxid);
 router.post('/accesos', postAcceso);
