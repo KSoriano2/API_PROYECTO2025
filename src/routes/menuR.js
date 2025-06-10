67e0eae0ffeff09f6fbb28edc973d337a817eee9
KSoriano2 <sorianog500@gmail.com>
2025-06-09 23:24:25 -0500

    Delete src/routes/menuR.js

--- src/routes/menuR.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { recuperarMenu} from "../controladores/menuC.js";
-
-
-const router = Router();
-
-router.post('/menu', recuperarMenu);
-
-export default router;
