620da985da087f06c5a1d79eee2907e94e4254e7
KSoriano2 <sorianog500@gmail.com>
2025-06-17 13:22:26 -0500

    Update citasC.js

--- src/controladores/citasC.js
+++ src/controladores/citasC.js
@@ -6,7 +6,8 @@ export const obtenerCitas = (req, resp)=>{
 
 export const getCitas=async(req, resp)=>{
 try{
-    const [result] = await conmysql.query(' select * from CITAS')
+    const {id_paciente}=req.body;
+    const [result] = await conmysql.query(' select * from CITAS where ID_PACIENTE=?', [id_paciente])
     resp.json({ cant: result.length, data: result }) 
 }catch(error){
     return resp.status(500).json({ message: "error en el servidor"})
