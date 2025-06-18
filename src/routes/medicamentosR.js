9822dcdda8a42fa41fab33dc5ad34eb5cd34a5fc
KSoriano2 <sorianog500@gmail.com>
2025-06-18 01:39:09 -0500

    Update medicamentosR.js

--- src/routes/medicamentosR.js
+++ src/routes/medicamentosR.js
@@ -1,27 +1,25 @@
 import { Router } from "express";
-import { getMedicamentos, getMedicamentosxid, postMedicamentos, putMedicamentos, patchMedicamentos, deleteMedicamentosxid } from "../controladores/medicamentosC.js";
+import {
+  getMedicamentos,
+  getMedicamentosxid,
+  postMedicamentos,
+  putMedicamentos,
+  patchMedicamentos,
+  deleteMedicamentosxid
+} from "../controladores/medicamentosC.js";
 import multer from "multer";
 
 const router = Router();
 
-const storage = multer.diskStorage({
-destination: (req, file, cb)=>{
-    cb(null, 'uploads'); //carpeta donde se guardan las imagenes
-},
-filename:(req, file, cb)=>{
-    cb(null, `${Date.now()}-${file.originalname}`);
-}
-})
-
-
-const upload = multer({storage});
-
-
+// Usamos memoryStorage porque subimos a Cloudinary directamente desde el buffer
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
 router.get('/medicamentos', getMedicamentos);
 router.get('/medicamentos/:id', getMedicamentosxid);
-router.post('/medicamentos', upload.single('imagen_medicamento'),postMedicamentos);
-router.put('/medicamentos', upload.single('imagen_medicamento'), putMedicamentos);
-router.patch('/medicamentos/:id',upload.single('imagen_medicamento'), patchMedicamentos);
-router.delete('/medicamentos/:id',deleteMedicamentosxid)
-export default router; 
+router.post('/medicamentos', upload.single('imagen_medicamento'), postMedicamentos);
+router.put('/medicamentos/:id', upload.single('imagen_medicamento'), putMedicamentos);  // <--- corregido
+router.patch('/medicamentos/:id', upload.single('imagen_medicamento'), patchMedicamentos);
+router.delete('/medicamentos/:id', deleteMedicamentosxid);
+
+export default router;
