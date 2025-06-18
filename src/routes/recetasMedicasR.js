893b774711e55ee65b0c18d91bec41cc9b835594
KSoriano2 <sorianog500@gmail.com>
2025-06-18 09:15:06 -0500

    Update recetasMedicasR.js

--- src/routes/recetasMedicasR.js
+++ src/routes/recetasMedicasR.js
@@ -1,13 +1,15 @@
 import { Router } from "express";
-import { getRecetasMedicas, getRecetasMedicasxid, postRecetasMedicas, putRecetasMedicas, patchRecetasMedicas, deleteRecetasMedicasxid } from "../controladores/recetasMedicasC.js";
+import { getRecetasPorPaciente,getRecetasMedicas, getRecetasMedicasxid, postRecetasMedicas, putRecetasMedicas, patchRecetasMedicas, deleteRecetasMedicasxid } from "../controladores/recetasMedicasC.js";
 
 
 const router = Router();
 
 router.get('/recetas_medicas', getRecetasMedicas);
 router.get('/recetas_medicas/:id', getRecetasMedicasxid);
+router.get('/paciente/:id', getRecetasPorPaciente);
 router.post('/recetas_medicas', postRecetasMedicas);
 router.put('/recetas_medicas', putRecetasMedicas);
 router.patch('/recetas_medicas/:id', patchRecetasMedicas);
 router.delete('/recetas_medicas/:id', deleteRecetasMedicasxid)
+
 export default router; 
