43a7c530b6d45023a7981f86136f32b443af1ae5
KSoriano2 <sorianog500@gmail.com>
2025-06-18 05:05:50 -0500

    Update medicosR.js

--- src/routes/medicosR.js
+++ src/routes/medicosR.js
@@ -13,7 +13,7 @@ const upload = multer({ storage });
 router.get('/medicos', getMedicos);
 router.get('/medicos/:id', getMedicosxid);
 router.post('/medicos',upload.single('imagen_medico') ,postMedicos);
-router.put('/medicos',upload.single('imagen_medico') ,putMedicos);
+router.put('/medicos/:id',upload.single('imagen_medico') ,putMedicos);
 router.patch('/medicos/:id',upload.single('imagen_medico') ,patchMedicos);
 router.delete('/medicos/:id' ,deleteMedicosxid)
 export default router; 
